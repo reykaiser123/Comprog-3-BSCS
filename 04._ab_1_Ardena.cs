876898092e9e8c65bb47da7972945b8cbc14825f
Ardeña, P <reynoldardena04@gmail.com>
2024-09-04 19:46:00 +0800

    Reynold Ardeña

---
+++ 04._ab_1_Ardena.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_lab_1_Ardena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the product you want to purchase: ");
+            string product = Console.ReadLine();
+
+            Console.Write("Enter the number of "+ product +  " you want to purchase: ");
+            int pnum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The total price of " + pnum + " " + product + ": " );
+            double pprice = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("the value of the converted price is: " + Math.Round(pprice) );
+
+
+        }
+    }
+}
